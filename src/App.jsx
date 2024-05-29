e700d0e16c58e160e48065268a2437fab1afa837
Riki Awaludin <rikiawaludin17@gmail.com>
2024-05-29 11:13:07 +0700

    checkpoint add data ditampilkan di console

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const groceryItems = [
+  {
+    id: 1,
+    name: 'Kopi Bubuk',
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: 'Gula Pasir',
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: 'Air Mineral',
+    quantity: 3,
+    checked: false,
+  },
+];
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+
+      <Header />
+
+      <Form />
+
+      <GroceryList />
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+function Header() {
+  return <h1>Catatan Belanjaku 📝</h1>
+}
+
+function Form() {
+
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    if(!name) return;
+
+    const newItem = {name, quantity, checked: false, id: Date.now()};
+    console.log(newItem);
+
+    setName('');
+    setQuantity(1);
+  }
+
+  const quantityNum = [...Array(20)].map((_, i) => (
+    <option value={i + 1} key={i + 1} > {i + 1} </option>
+  ));
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+        <h3>Hari ini belanja apa kita?</h3>
+        <div>
+          <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+            {quantityNum}
+          </select>
+          <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
+          <button >Tambah</button>
+        </div>
+    </form>
+  );
+}
+
+function GroceryList() {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {groceryItems.map((item) => (
+            <Item item={item} key={item.id} />
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button>Bersihkan Daftar</button>
+      </div>
+    </>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" checked={item.checked}  />
+      <span style={item.checked ? { textDecoration: 'line-through' } : {}}>{item.quantity} {item.name} </span>
+      <button>&times;</button>
+    </li>
+  );
+}
+
+function Footer() {
+  return(
+      <footer className="stats">
+        Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
+      </footer>
+  );
+}
